2de2a0cfa815c7280bb579b8aea7fde267f44487
DongGreat <sinzzanggu3@gmail.com>
2021-05-01 00:05:52 +0900

    frag_1.xml의 쓸모 없는 버튼 지우고 ListView 추가 MyData.java 파일 추가(ListView에 들어갈 데이터)

---
+++ app/src/main/java/com/example/teamproject2/MyData.java
@@ -0,0 +1,12 @@
+package com.example.teamproject2;
+
+public class MyData {
+
+    public int icon;
+    public String name;
+
+    public MyData(int icon, String name) {
+        this.icon = icon;
+        this.name = name;
+    }
+}
